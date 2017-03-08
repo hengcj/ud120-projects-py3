b66f44fcfa5e3597ca54dd39f32f6cbad786494c
hengchunjie <winfringe@gmail.com>
2017-03-08 15:14:19 +0800

    dt test

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
+
+# print(len(features_train[0]))
+t0 = time()
+clf1 = tree.DecisionTreeClassifier(min_samples_split=40)
+clf1.fit(features_train, labels_train)
+print('fit time:', round(time() - t0, 3), 's')
+
+t1 = time()
+score1 = clf1.score(features_test, labels_test)
+print(score1)
+print('predict time:', round(time() - t1, 3), 's')
+
+# t0 = time()
+# clf2 = tree.DecisionTreeClassifier(min_samples_split=50)
+# clf2.fit(features_train, labels_train)
+# print('fit time:', round(time() - t0, 3), 's')
+#
+# t1 = time()
+# score2 = clf2.score(features_test, labels_test)
+# print(score2)
+# print('predict time:', round(time() - t1, 3), 's')
 
 #########################################################
 
